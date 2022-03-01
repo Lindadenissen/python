307f9db1e21e8e7222981d17443e4d46def3a212
Linda Denissen <78142605+Lindadenissen@users.noreply.github.com>
2022-03-01 22:13:54 +0100

    Add files via upload

---
+++ strings/main.py
@@ -0,0 +1,24 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+scoring_player1 = "Ruud Gullit"
+scoring_player2 = "Marco van Basten"
+goal_0 = 32
+goal_1 = 54
+
+scorers = f'{scoring_player1 } {goal_0}, {scoring_player2 } {goal_1}'
+
+report = f'{scoring_player1} scored in the {goal_0}nd minute \n{scoring_player2} scored in the {goal_1}th minute'
+
+print(scorers)
+print(report)
+
+player = "Frank Rijkaard"; 
+
+first_name = player[0:5]; last_name = player.find("Rijkaard"); name_short = player[0:1:6]
+
+print(player, first_name, last_name, name_short)
+
+
